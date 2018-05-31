5e45cf8499cda1b41a8e1c7ea55bd426899b6aed
Jesse Gibson <JesseTheGibson@gmail.com>
2018-05-30 22:13:18 -0600

    Augment babel with webpack
    Using webpack allows me to take advantage of the loaders. Specifically,
    CSS.

---
+++ packages/desktop/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+// eslint-disable-next-line no-process-env
+const { NODE_ENV } = process.env;
+const mode = NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = {
+  mode,
+  entry: {
+    app: path.join(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  target: 'electron-renderer',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
