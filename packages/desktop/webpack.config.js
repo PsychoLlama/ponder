6533429e069295e6397c856c64d2142a49418260
Jesse Gibson <JesseTheGibson@gmail.com>
2018-05-30 22:40:27 -0600

    Add css loaders
    In my hack branch these loaders were sorely needed for CodeMirror and
    other friends.

--- packages/desktop/webpack.config.js
+++ packages/desktop/webpack.config.js
@@ -19,9 +19,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
