1e3c25731f1b73e13a333b835d602a32c77c86b6
Jesse Gibson <JesseTheGibson@gmail.com>
2018-05-30 22:49:25 -0600

    Move styles from html file into dedicated file
    I don't like css in my html. I don't like css in my css either, but it's
    the lesser of two evils (IMHO).

--- packages/desktop/src/index.js
+++ packages/desktop/src/index.js
@@ -1,5 +1,7 @@
 import DOM from 'react-dom';
 import React from 'react';
 
+import './global.css';
+
 const container = document.getElementById('app-root');
 DOM.render(<div>Hello from React land.</div>, container);
