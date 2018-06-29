fa372edebbe76836e7c64c5bddc6e203c8f7c4d5
Jesse Gibson <JesseTheGibson@gmail.com>
2018-06-28 20:59:16 -0600

    Upgrade deps
    Not all dependencies support babel v7. Specifically a transitive
    dependency of Jest is causing a babel issue. Everything builds fine, but
    the tests fail with an error.
    
    This should be resolved once all dependencies have released a version
    with the updated `@babel/env` package. If `babel-core` exists anywhere,
    something will break.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   babelrcRoots: 'workspaces/*',
-  presets: ['@babel/env', '@babel/preset-react'],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: ['babel-plugin-transform-class-properties'],
 };
