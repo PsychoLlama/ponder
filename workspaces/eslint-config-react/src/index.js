121cacf30a63d04203ee14d1f87c3bec0fd1831e
Jesse Gibson <JesseTheGibson@gmail.com>
2018-11-18 13:06:45 -0700

    Add shared eslint react config
    I need it for `@ponder/ui`. Amazing how many things I'm needing to pull
    into a package. Well, not really "needing", but I can't just settle for
    something half-baked, can I?

---
+++ workspaces/eslint-config-react/src/index.js
@@ -0,0 +1,28 @@
+// @flow
+const { version: flowVersion } = require('flow-bin/package');
+const { version: reactVersion } = require('react/package');
+
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+
+  // HACK: this assumes the plugin is hoisted and visible to ESLint.
+  // Can be cleanly implemented after eslint/issues/10125.
+  plugins: ['react'],
+  extends: ['plugin:react/recommended'],
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  settings: {
+    react: {
+      version: reactVersion,
+      flowVersion,
+    },
+  },
+};
