0247eb79064d234e62f932e0ef35b6ce01e569a3
Jesse Gibson <JesseTheGibson@gmail.com>
2019-03-25 17:07:53 -0600

    Attempt to display the note title
    Renders the note's title in the note title input. Next up, add the
    ability to rename a note.

--- workspaces/desktop/src/components/Note.js
+++ workspaces/desktop/src/components/Note.js
@@ -1,8 +1,10 @@
 // @flow
 import { Input, MarkdownEditor } from '@ponder/ui';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 import React from 'react';
 
+import type { ReduxState } from '../types/redux-store';
 import { translate } from '../utils/translation';
 
 const Container = styled.article`
@@ -13,17 +15,39 @@ const Container = styled.article`
 
 const noop = () => {};
 
-type Props = {};
+type Props = {
+  title: string,
+};
 
 export class Note extends React.Component<Props> {
   render() {
+    const { title } = this.props;
+
     return (
       <Container>
-        <Input placeholder={translate('Untitled Note')} onChange={noop} />
+        <Input
+          placeholder={translate('Untitled Note')}
+          onChange={noop}
+          value={title}
+        />
+
         <MarkdownEditor />
       </Container>
     );
   }
 }
 
-export default Note;
+export const mapStateToProps = ({ navigation, notebooks }: ReduxState) => {
+  const { selectedNoteId } = notebooks;
+  const note = navigation.items.find(note => note.id === selectedNoteId);
+
+  if (!note) {
+    throw new Error(`No note has been selected.`);
+  }
+
+  return {
+    title: note.title,
+  };
+};
+
+export default connect(mapStateToProps)(Note);
--- workspaces/desktop/src/components/__tests__/Note.test.js
+++ workspaces/desktop/src/components/__tests__/Note.test.js
@@ -2,10 +2,15 @@
 import { renderer } from '@ponder/test-utils';
 import { Input } from '@ponder/ui';
 
-import { Note } from '../Note';
+import { Note, mapStateToProps } from '../Note';
+import { selector } from '../../utils/testing';
 
 describe('Note', () => {
-  const setup = renderer(Note, {});
+  const setup = renderer(Note, {
+    getDefaultProps: () => ({
+      title: 'Note title',
+    }),
+  });
 
   it('renders', () => {
     expect(setup).not.toThrow();
@@ -15,5 +20,35 @@ describe('Note', () => {
     const { output } = setup();
 
     output.find(Input).simulate('change', 'content');
+  });
+
+  it('shows the note title', () => {
+    const { output, props } = setup();
+    const note = output.find(Input);
+
+    expect(note.prop('value')).toBe(props.title);
+  });
+
+  describe('mapStateToProps', () => {
+    const select = selector(mapStateToProps, {});
+
+    it('grabs the note title', () => {
+      const { props } = select(state => {
+        const note = { type: 'note', id: 'note', title: 'Title' };
+        state.notebooks.selectedNoteId = note.id;
+        state.navigation.items = [note];
+      });
+
+      expect(props.title).toBe('Title');
+    });
+
+    it('throws an error if there is no selected note', () => {
+      const fail = () =>
+        select(state => {
+          state.notebooks.selectedNoteId = null;
+        });
+
+      expect(fail).toThrow(/selected/i);
+    });
   });
 });
