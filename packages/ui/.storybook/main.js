98df771a735b94297c537b15082cdb95542fea26
Jesse Gibson <JesseTheGibson@gmail.com>
2020-05-17 15:05:38 -0600

    Enable TypeScript in Storybook
    Apparently I didn't fix this. Whoops.

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,3 +1,14 @@
 module.exports = {
-  stories: ['../src/stories/**/*.stories.js'],
+  stories: ['../src/stories/**/*.stories.ts{x,}'],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      use: { loader: require.resolve('ts-loader') },
+    });
+
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    return config;
+  },
 };
