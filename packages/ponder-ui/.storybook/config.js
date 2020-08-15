71f7f267efb2d4780070d808f0dd1fc82562c7c7
Jesse Gibson <JesseTheGibson@gmail.com>
2020-08-15 16:38:40 -0600

    Unbreak storybook
    They keep changing their API. This is using their "legacy addons API".
    I should migrate to the new system, but I don't care enough right now.
    That's a problem for another day.

--- packages/ponder-ui/.storybook/config.js
+++
@@ -1,6 +0,0 @@
-import { addDecorator } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
-import centered from '@storybook/addon-centered/react';
-
-addDecorator(withKnobs);
-addDecorator(centered);
